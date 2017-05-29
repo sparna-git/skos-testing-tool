b109ab56b883ef97c4a11ab6f283e9ed4cfd4a40
clarvie <236tech@gmail.com>
2017-05-29 15:42:37 +0200

    -

---
+++ src/main/java/fr/sparna/validator/ValidateSkosFile.java
@@ -0,0 +1,19 @@
+package fr.sparna.validator;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Collection;
+
+import org.openrdf.model.impl.URIImpl;
+
+import at.ac.univie.mminf.qskos4j.QSkos;
+import at.ac.univie.mminf.qskos4j.issues.Issue;
+import at.ac.univie.mminf.qskos4j.progress.ConsoleProgressMonitor;
+import at.ac.univie.mminf.qskos4j.util.vocab.SkosOntology;
+
+
+
+public class ValidateSkosFile {
+	
+	
+}
